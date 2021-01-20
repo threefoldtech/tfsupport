eb3546fc89aa842cfb348a38e92af6e4f6ffb72a
hamdy <hamdy.a.farag@gmail.com>
2021-01-20 11:22:58 +0200

    filter people by fairswap only

--- src/components/custom/Cards/PostListItem.vue
+++ src/components/custom/Cards/PostListItem.vue
@@ -109,7 +109,7 @@ export default {
         return []
       }
       memberships.forEach(function(membership){
-        if(["foundation", "tech"].includes(membership.title)){
+        if(["fairswap"].includes(membership.title)){
           res.push(membership)
         }
       });
