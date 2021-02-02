1d0b7e66e517301fc22212d62f0e5837d35cb05e
Vilnite <vilnitekristine@gmail.com>
2021-02-02 17:09:18 +0200

    footer shortened

--- src/components/marketing/sections/cta-sections/Footer.vue
+++ src/components/marketing/sections/cta-sections/Footer.vue
@@ -6,7 +6,7 @@
   <footer class="bg-white">
     <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="space-y-8 xl:col-span-1">
+       <!-- <div class="space-y-8 xl:col-span-1">
           <g-image
             class="h-10"
             src="/img/"
@@ -118,8 +118,8 @@
                 />
               </svg>
             </a>
-          </div>
-        </div>
+          </div> 
+        </div> -->
         <div
           class="mt-12 grid md:grid-cols-3 sm:grid-cols-2 xl:mt-0 xl:col-span-2"
         >
@@ -130,7 +130,7 @@
             :key="footerLink.title"
             class="md:grid md:grid-cols-3"
           >
-            <div>
+            <!-- <div>
               <h4
                 class="text-sm leading-5 font-semibold text-gray-400 tracking-wider uppercase"
               >
@@ -155,16 +155,16 @@
                   </a>
                 </li>
               </ul>
-            </div>
+            </div> -->
           </div>
-        </div>
-      </div>
+        </div> 
+      </div> 
       <div class="mt-12 border-t border-gray-200 pt-8">
         <p class="text-base leading-6 text-gray-400 xl:text-center">
           &#xA9; 2021, ThreeFold, All rights reserved.
         </p>
-      </div>
-    </div>
+      </div> 
+    </div>  
   </footer>
 </template>
 
